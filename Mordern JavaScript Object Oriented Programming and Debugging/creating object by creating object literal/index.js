3da24e239cf8c9e60c466c4b47cb82b1e3a2ba83
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-19 20:12:54 +0600

    Update index.js

--- Mordern JavaScript Object Oriented Programming and Debugging/creating object by creating object literal/index.js
+++ Mordern JavaScript Object Oriented Programming and Debugging/creating object by creating object literal/index.js
@@ -1,9 +1,17 @@
-//by object literal
-//what is object literal
+// by object literal
+// what is object literal
 // how to create object by object literal
 
-let person={
-   
-
-
-}
+let person = {
+    first_name: 'Arnob',
+    last_name: 'Dev Nath',
+    age: 22,
+    city: 'Chittagong',
+    isBangladeshi: true, // key:value pair is called property
+    getName: function() { // function is called method of object
+      console.log(`My full name is ${this.first_name} ${this.last_name}`); // this is a keyword which refers to the current object
+    }
+  };
+  
+  // Example usage:
+  person.getName(); // My full name is Arnob Dev Nath
