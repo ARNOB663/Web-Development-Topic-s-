92fcb510882a5a2442f6da4ea40a41940e86709d
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-26 22:17:02 +0600

    Update index.js

---
+++ Class Variable Function Object/index.js
@@ -0,0 +1,14 @@
+//
+class person {
+ 
+    first_name = "Arnob";
+    last_name = "Dev Nath";
+    age=34;
+    city="Dhaka";
+    isBangladeshi=true;
+    getName=()=>
+    {
+        return `My name is  ${this.first_name} ${this.last_name}`;
+    }
+    
+}
