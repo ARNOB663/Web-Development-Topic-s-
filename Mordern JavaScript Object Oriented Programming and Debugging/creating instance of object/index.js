104a8b4db1c152b53d607da0559df1f5e68b8781
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-26 21:57:48 +0600

    Update index.js

--- Mordern JavaScript Object Oriented Programming and Debugging/creating instance of object/index.js
+++ Mordern JavaScript Object Oriented Programming and Debugging/creating instance of object/index.js
@@ -1,3 +1,9 @@
+// creating instance of object
+// 1. Create a function named person
+// 2. Add some properties like first_name, last_name, age, city, isBangladeshi
+// 3. Add a method named getName which will return the full name of the person
+// 4. Create an instance of the person object and call the getName method
+// 5. Print the full name of the person
 function person()
 {
     this.first_name = "John";
