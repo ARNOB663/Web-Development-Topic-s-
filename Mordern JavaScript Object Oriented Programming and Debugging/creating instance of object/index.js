f927ca2bb63c61bcac53ff8cd890b44da5963a66
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-26 21:56:14 +0600

    Update index.js

--- Mordern JavaScript Object Oriented Programming and Debugging/creating instance of object/index.js
+++ Mordern JavaScript Object Oriented Programming and Debugging/creating instance of object/index.js
@@ -1,9 +1,17 @@
-function object()
+function person()
 {
     this.first_name = "John";
     this.last_name = "Doe";
     this.age=34;
     this.city="New York";
     this.isBangladeshi=true;
+    this.getName=()=>
+    {
+        return this.first_name+" "+this.last_name;
+    }
 
 }
+
+let personInstance = new person();
+
+console.log(personInstance.getName());
