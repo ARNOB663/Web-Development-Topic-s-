8abbf5f42f61dce3bebb2c9eab71727d6380a007
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-27 11:50:55 +0600

    Update main.js

--- NODE JS/Source/main.js
+++ NODE JS/Source/main.js
@@ -1,10 +1,9 @@
-var http = require('http');
+var http=require("http");
 
-var server = http.createServer(function (req, res){
+var server=http.createServer(function (req,res){
 
-res.end("hello world");
 
-})
+});
 
 server.listen(5050);
-console.log("server is running")
+console.log("Server started on port 5050");
