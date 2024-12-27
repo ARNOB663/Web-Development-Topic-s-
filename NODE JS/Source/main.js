e2a9f5481820c8c8f31c8c6a242d4c8905bd89f4
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-27 12:16:22 +0600

    Update main.js

--- NODE JS/Source/main.js
+++ NODE JS/Source/main.js
@@ -3,14 +3,21 @@ var http=require("http");
 var server=http.createServer(function (req,res){
 
 if(req.url=="/"){
-
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('<h1>this is home page</h1>>')
+    res.end();
 }
 else if(req.url=="/about")
 {
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('<h1>this is about page</h1>>')
+    res.end();
 
 }
 else if(req.url=="/contact"){
-    
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('<h1>this is contact page</h1>>')
+    res.end();
 }
 
 });
