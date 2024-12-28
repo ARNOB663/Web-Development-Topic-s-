f4370bc5f9c39c1ed4ff0ba3331cb8f883ab0904
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-28 20:35:22 +0600

    creating html file

---
+++ NODE JS/FS_module/index.js
@@ -0,0 +1,11 @@
+
+import http from 'http';
+var fs=require('fs');
+//var http=require('http');
+
+var server=http.createServer(function (req,res){
+
+});
+
+server.listen(5050);
+console.log("Server started on port 5050");
