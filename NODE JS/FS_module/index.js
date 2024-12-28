b4e6289ef769f6677d3d1de5628e896755da1d6d
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-28 20:44:40 +0600

    update

--- NODE JS/FS_module/index.js
+++ NODE JS/FS_module/index.js
@@ -1,20 +1,21 @@
 
-import http from 'http';
 var fs=require('fs');
-//var http=require('http');
+var http=require('http');
 
 var server=http.createServer(function (req,res){
 
     if(req.url=="/")
     {   //asy method
         fs.readFile('Home.html',function (error,data){
-
+            res.writeHead(200,{'content-type':'text/html'})
+          res.write(data);
+          res.end();
         })
     }
-    
+
 
 
 });
 
-server.listen(5050);
-console.log("Server started on port 5050");
+server.listen(5151);
+console.log("Server started on port 5151");
