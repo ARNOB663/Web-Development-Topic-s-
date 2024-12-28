ffee414611ffe0ff1376875f3b67b72337298814
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-28 11:11:37 +0600

    a

--- NODE JS/URL Module/main.js
+++ NODE JS/URL Module/main.js
@@ -5,8 +5,15 @@ var server = http.createServer(function(req,res){
     var myURL="http://rabbil.com/blog.html?year=2020&month=july";
      var myURLObj = URL.parse(myURL,true);
      var myHost=myURLObj.host;
-     var myPathName=myURLObj.path;
+     var myPathName=myURLObj.pathname;
      var mySearchName=myURLObj.search;
+  res.writeHead(200,{'Content-Type':'text/html'});
+     res.write(myHost);
+     res.write("<br>");
+     res.write(myPathName);
+     res.write("<br>");
+     res.write(mySearchName);
+     res.end();
 })
 
 server.listen(5050);
