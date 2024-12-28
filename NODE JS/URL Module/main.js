2ef5618b5516ea7adf663a1d45f43cd243307d7b
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-28 10:32:31 +0600

    Update main.js

--- NODE JS/URL Module/main.js
+++ NODE JS/URL Module/main.js
@@ -2,7 +2,12 @@ var http=require("http")
 var URL=require('url');
 
 var server = http.createServer(function(req,res){
-    var myURL="http://rabbil.com/blog.html?year=2020&month=july;\n"
-
+    var myURL="http://rabbil.com/blog.html?year=2020&month=july";
+     var myURLObj = URL.parse(myURL,true);
+     var myHost=myURLObj.host;
+     var myPathName=myURLObj.path;
 
 })
+
+server.listen(5050);
+console.log("Server running on port 5050");
