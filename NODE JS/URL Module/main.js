b44b24ecef67065b30a766481b7e4f867d89147e
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-12-30 20:05:13 +0600

    Update main.js

--- NODE JS/URL Module/main.js
+++ NODE JS/URL Module/main.js
@@ -9,6 +9,7 @@ var server = http.createServer(function(req,res){
      var mySearchName=myURLObj.search;
   res.writeHead(200,{'Content-Type':'text/html'});
      res.write(myHost);//this used for 
+     
      res.write("<br>");//
      res.write(myPathName);//
      res.write("<br>");//
