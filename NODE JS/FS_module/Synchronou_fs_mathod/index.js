0b14821b8d3ee1488db9d17d4846d3b9bcaa71be
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-01 22:12:32 +0600

    Sync file creation

---
+++ NODE JS/FS_module/Synchronou_fs_mathod/index.js
@@ -0,0 +1,40 @@
+
+var fs=require('fs');
+var http=require('http');
+
+var server=http.createServer(function (req,res){
+
+    if(req.url=="/")
+    {
+
+        fs.writeFile("demo.txt","welcome to Node JS",function(error,data){
+            if(error)
+            {
+                res.writeHead(404,{'Content-Type':'text/plain'});
+                res.write("File Write Failed.");
+                res.end();
+            }
+            else
+            {
+                res.writeHead(200,{'content-type':'text/html'});
+                res.write("File Write Success!\n");
+
+            }
+
+        })
+
+        //asy method
+        // fs.readFile('Home.html',function (error,data){
+        //     res.writeHead(200,{'content-type':'text/html'})
+        //   res.write(data);
+        //   res.end();
+        // })
+        // let mydata = fs.readFileSync('Home.html',)
+        // res.writeHead(200,{'content-type':'text/html'})
+        // res.write(data);
+        // res.end();
+    }
+});
+
+server.listen(5152);
+console.log("Server started on port 5151");
