500d9b9efebcdba4f4f7823cd0979f21625ea78e
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-01 20:50:57 +0600

    Update index.js

--- NODE JS/FS_module/asynchronous_fs_mathod/index.js
+++ NODE JS/FS_module/asynchronous_fs_mathod/index.js
@@ -29,10 +29,10 @@ var server=http.createServer(function (req,res){
         //   res.write(data);
         //   res.end();
         // })
-        let mydata = fs.readFileSync('Home.html',)
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write(data);
-        res.end();
+        // let mydata = fs.readFileSync('Home.html',)
+        // res.writeHead(200,{'content-type':'text/html'})
+        // res.write(data);
+        // res.end();
     }
 });
 
