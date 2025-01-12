32c6dc79174ab188c19a3b173fcdb59bf0f2ba7d
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-12 15:37:15 +0600

    Update index.js

---  Express_JS/index.js
+++  Express_JS/index.js
@@ -2,23 +2,36 @@ var express= require('express')
 const res = require("express/lib/response");
 
 app= express();
+// res.send() => responce Body
+//res.end() => responce ending point...
 
-app.get("/",function (req,res){
-    res.send("hello world");
+//simple String Responce
+app.get("/one",function (req,res){
+    //res.send("this is simple string responce");
+    res.end('Welcome');
 })
 
-app.post("/about",function(req,res){
-    res.send("about");
+app.post("/two",function (req,res){
+   // res.send("This is simple string responce");
+    res.end('Welcome');
 })
 
-app.put("/contact",function (req,res){
-    res.send("contact");
+app.get('/three',function (req,res){
+    res.status(401).end('Unauthorized');
 })
 
-app.delete("/term",function (req,res){
-    res.send("term");
-})
 
+
+
+// app.post("/about",function(req,res){
+//     res.send("about");
+// })
+// app.put("/contact",function (req,res){
+//     res.send("contact");
+// })
+// app.delete("/term",function (req,res){
+//     res.send("term");
+// })
 app.listen(8080,function (){
     console.log("Server running on port 8080");
 })
