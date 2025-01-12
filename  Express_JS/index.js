4f6bf88f542b705d0da24227ade469592bdb8fb3
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-12 11:56:36 +0600

    Update index.js

---  Express_JS/index.js
+++  Express_JS/index.js
@@ -1,12 +1,16 @@
 var express= require('express')
+const res = require("express/lib/response");
 
-app= express()
-
+app= express();
 
 app.get("/",function (req,res){
-    res.send("hello world")
+    res.send("hello world");
+})
+
+app.get("/about",function(req,res){
+    res.send("about");
 })
 
 app.listen(8080,function (){
-    console.log("Server running on port 8080")
+    console.log("Server running on port 8080");
 })
