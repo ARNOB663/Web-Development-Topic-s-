c0e9be950172f3caaa11b43f9df098b54a331bf0
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-13 11:34:00 +0600

    Express_js

---  Express_JS/index.js
+++  Express_JS/index.js
@@ -43,6 +43,45 @@ app.get('/four',function (req,res){
     res.json(myJson);
 })
 
+//download
+app.get("/five",function (req,res){
+    res.download("./upload/abc.jpg")
+})
+//url redirect
+app.get("/bd",function (req,res){
+    res.redirect("http://localhost:8080/usa")//redirectd to usa
+})
+
+app.get("/usa",function (req,res){
+  res.send("This is usa");
+})
+
+app.get("/six",function (req,res){
+    //put Data in header
+    // responce Header
+    res.append("name","arnob")
+    res.append("email","arnob229x@gmail.com")
+    res.append("name","arnob229x@gmail.com")
+    res.append("pass","123456789")
+
+    res.status(201).end("hello world");
+})
+//responce
+app.get("/seven",function (req,res){
+    res.cookie('name','arnob')
+    res.cookie('email','arnob229x@gmail.com')
+    res.cookie('city','ctg')
+    res.end("cookies set for arnob")
+})
+
+app.get("/eight",function (req,res){
+   res.clearCookie('name');
+   res.clearCookie('email');
+
+    res.end("clear cookies")
+})
+
+
 
 
 // app.post("/about",function(req,res){
@@ -54,6 +93,8 @@ app.get('/four',function (req,res){
 // app.delete("/term",function (req,res){
 //     res.send("term");
 // })
+
+//
 app.listen(8080,function (){
     console.log("Server running on port 8080");
 })
