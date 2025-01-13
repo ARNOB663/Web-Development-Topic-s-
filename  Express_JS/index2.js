a84439f378413692796ea40e0d8433ae256aa63d
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-13 17:16:17 +0600

    Update index2.js

---  Express_JS/index2.js
+++  Express_JS/index2.js
@@ -1,12 +1,19 @@
 var express = require('express')
+const {query} = require("express");
 
 app=express();
 
 
 app.get('/',function(req,res){
+  //Get Request with URL Query
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
 
-    res.end('this is simple get rq')
+
+    res.end(firstName+lastName)
+
 })
+
 
 app.listen(3000,()=>{
     console.log("Server started on port 3000");
