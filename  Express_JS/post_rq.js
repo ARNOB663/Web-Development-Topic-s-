900d9762067b8da41754299f714b33e0af6a4683
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-14 19:07:48 +0600

    Update post_rq.js

---  Express_JS/post_rq.js
+++  Express_JS/post_rq.js
@@ -9,12 +9,16 @@ app.post('/',function (req,res){
      let firstName = req.query.firstName;
     let LastName = req.query.lastName;
 
-    res.send({firstName:firstName,LastName:LastName});
+    //Post  RQ with header properties
+
+    let username = req.header('username')
+    let password = req.header('password')
+
+   // res.send({firstName:firstName,LastName:LastName});
+
+    res.send("User Name :"+ username +" password "+ password)
 
 })
-
-
-
 
 app.listen(8000,function (){
     console.log("Express server listening on 8000");
