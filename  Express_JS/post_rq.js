d229eabf47c493c9324801787fa4146086cd2b8a
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-14 18:49:20 +0600

    Update post_rq.js

---  Express_JS/post_rq.js
+++  Express_JS/post_rq.js
@@ -1,14 +1,23 @@
 var express=require('express');
 app=express();
-
+//This is
 app.post('/',function (req,res){
-    res.send("This is simple post request");
+
+    1
+    //res.send("This is simple post request");
+   //Post Request With URL Query
+     let firstName = req.query.firstName;
+    let LastName = req.query.lastName;
+
+    res.send({firstName:firstName,LastName:LastName});
+
 })
+
 
 
 
 app.listen(8000,function (){
     console.log("Express server listening on 8000");
 
-    
+
 });
