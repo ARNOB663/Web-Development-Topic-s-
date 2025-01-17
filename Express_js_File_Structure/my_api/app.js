6abb5a72ed9b84725d474643843f8465cf886fde
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-17 21:58:49 +0600

    Security Middleware implementation

--- Express_js_File_Structure/my_api/app.js
+++ Express_js_File_Structure/my_api/app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = require("./src/routes/api");
-
 const app= new express();
+//Security Middleware import
+  const rateLimit = require('express-rate-limit')
+  const helmet =  require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss=    require('xss-clean')
+const hpp =    require('hpp')
+ const cors =   require('cors')
+
+//Security Middleware implementation
+  app.use(cors());
+  app.use(helmet());
+  app.use(mongoSanitize());
+  app.use(hpp);
+  app.use(xss());
+
+//Request Rate Limiter 
+
+const limiter = rateLimit({
+
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+    });
+
+    app.use(limiter);//this is the middleware that will be used for all the routes that start with /api/v1
+
 app.use("/api/v1",router);//this is the middleware that will be used for all the routes that start with /api/v1
+
 
 //Undefined route
 app.use('*',(req,res)=>{
--- Express_js_File_Structure/my_api/src/routes/api.js
+++ Express_js_File_Structure/my_api/src/routes/api.js
@@ -4,11 +4,8 @@
 
  //This is my 1st get routing
  router.get("/hello-get",HelloController.HelloGet)
+//This is my 2nd post routing
  router.post("/hello-post",HelloController.HelloPost)
-
-
-
-
 
  module.exports = router;
 
