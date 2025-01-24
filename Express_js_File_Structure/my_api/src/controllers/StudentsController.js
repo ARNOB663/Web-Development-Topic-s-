93527b7de9ef25f392151d9ecdb2718b11c89685
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-24 21:49:50 +0600

    Update StudentsController.js

--- Express_js_File_Structure/my_api/src/controllers/StudentsController.js
+++ Express_js_File_Structure/my_api/src/controllers/StudentsController.js
@@ -6,8 +6,8 @@ const StudentsModel = require('../models/StudentModel.js');
 //C = Create
 exports.InsertStudent = async (req, res) => {
     try {
-        let reqBody = req.body;
-        let data = await StudentsModel.create(reqBody);
+        let reqBody = req.body;//This will fetch the data from the body of the request 
+        let data = await StudentsModel.create(reqBody);//This will insert the data into the database
         res.status(200).json({ status: 'success', data: data });
     } catch (err) {
         res.status(400).json({ status: 'failed', data: err });
@@ -16,11 +16,12 @@ exports.InsertStudent = async (req, res) => {
 //R = Read 
 exports.ReadStudent = async (req, res) => {
     try {
-        let Query = {};
-        let projection = "Name Roll Class";
+        let Query = {};//Empty Object means all data will be fetched from the database 
+        let projection = "Name Roll Class";//This will only fetch Name, Roll and Class from the database and will not fetch Remarks
         let data = await StudentsModel.find(Query, projection);
         res.status(200).json({ status: "success", data: data });
     } catch (err) {
         res.status(400).json({ status: "failed", data: err });
     }
 }
+//U = Update
