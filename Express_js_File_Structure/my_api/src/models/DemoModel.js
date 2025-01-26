5c95c6f704b15ce730ae2d5b8326445c41ac63a5
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-26 19:37:46 +0600

    Update DemoModel.js

--- Express_js_File_Structure/my_api/src/models/DemoModel.js
+++ Express_js_File_Structure/my_api/src/models/DemoModel.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const DataSchema = new mongoose.Schema({
+    //Schema Types: and default values
  Name:String,
- Roll:Number,
- Class:String,
-    Remarks:String,
+ Roll:{type:Number,default:null},
+ Class:{type:String,default:"N/A"},
+    Remarks:{type:String,default:"Good"},
     Adult:Boolean,
-    Comments:[],
-    details:{}
-
-
+    Comments:[ ],
+    details:{ },
+    dob:{type: Date, default: Date.now},
 })
+const DemoModel =  mongoose.model('students',DataSchema);
