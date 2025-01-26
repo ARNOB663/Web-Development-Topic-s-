ea3bb78c20841fd44d90963f9d55a472d281f16e
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-27 00:00:11 +0600

    Update StudentModel.js

--- Express_js_File_Structure/my_api/src/models/StudentModel.js
+++ Express_js_File_Structure/my_api/src/models/StudentModel.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
 const DataSchema = mongoose.Schema({
+  Name: String,
+  // Custom error message Validation
+  Roll: {
+    type: Number,
+    min: [6, "Roll number must be at least 6, but you have entered = {VALUE}"], // Custom error message Validation for min number of digits
+    max: [12, "Roll number must be under 12 digits, but you have entered = {VALUE}"], // Custom error message Validation for max number of digits
+  },
+  Class: String,
+  Remarks: { type: String, default: "Not Available" }, // default value set
+}, { versionKey: false });
 
-Name:String,
-Roll:String,
-Class:String,
-Remarks:{type:String,default:"Not Available"},
-
-},{versionKey:false});
-
-const StudentsModel = mongoose.model('students',DataSchema);
+const StudentsModel = mongoose.model('students', DataSchema);
 
 module.exports = StudentsModel;
