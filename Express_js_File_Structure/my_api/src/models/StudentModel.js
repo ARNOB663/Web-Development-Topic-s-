11b37dce1b968db1ccaebbb76e1d1236bfae38e4
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-27 21:12:45 +0600

    Update StudentModel.js

--- Express_js_File_Structure/my_api/src/models/StudentModel.js
+++ Express_js_File_Structure/my_api/src/models/StudentModel.js
@@ -8,6 +8,16 @@ const DataSchema = mongoose.Schema({
     min: [6, "Roll number must be at least 6, but you have entered = {VALUE}"], // Custom error message Validation for min number of digits
     max: [12, "Roll number must be under 12 digits, but you have entered = {VALUE}"], // Custom error message Validation for max number of digits
   },
+  Mobile2:{
+    type: String,
+    validate:{
+      validator: function(value){
+        return /^\d{11}$/.test(value); // Regular expression for 11 digits
+      },
+      message: "Mobile number must be exactly 11 digits"
+    }
+
+  },
   Class: String,
   Mobile: {
     type: String,
@@ -23,7 +33,8 @@ const DataSchema = mongoose.Schema({
   }
   },
   Remarks: { type: String, default: "Not Available" }, // default value set
-}, {versionKey:false });
+},
+ {versionKey:false });
 
 const StudentsModel = mongoose.model('students', DataSchema);
 
