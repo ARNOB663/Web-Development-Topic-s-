6c4b78e75a77c728898ae4d168bf053c7cee869e
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-28 21:03:13 +0600

    Update StudentModel.js

--- Express_js_File_Structure/my_api/src/models/StudentModel.js
+++ Express_js_File_Structure/my_api/src/models/StudentModel.js
@@ -4,35 +4,35 @@ const DataSchema = mongoose.Schema({
   Name: String,
   // Custom error message Validation
   Roll: {
-    type: Number,
-    min: [6, "Roll number must be at least 6, but you have entered = {VALUE}"], // Custom error message Validation for min number of digits
-    max: [12, "Roll number must be under 12 digits, but you have entered = {VALUE}"], // Custom error message Validation for max number of digits
-  },
-  Mobile2:{
     type: String,
-    validate:{
-      validator: function(value){
-        return /^\d{11}$/.test(value); // Regular expression for 11 digits
-      },
-      message: "Mobile number must be exactly 11 digits"
-    }
+    // min: [6, "Roll number must be at least 6, but you have entered = {VALUE}"], // Custom error message Validation for min number of digits
+    // max: [12, "Roll number must be under 12 digits, but you have entered = {VALUE}"], // Custom error message Validation for max number of digits
+  },
+  // Mobile2:{
+  //   type: String,
+  //   validate:{
+  //     validator: function(value){
+  //       return /^\d{11}$/.test(value); // Regular expression for 11 digits
+  //     },
+  //     message: "Mobile number must be exactly 11 digits"
+  //   }
 
-  },
+  // },
   Class: String,
-  Mobile: {
-    type: String,
-    validate: {
-      validator: function(value) {
-       if (value.length === 11) {
-          return true;
-        } else {
-          return false;
-      }
-    },
-    message: "Mobile number must be exactly 11 digits"
-  }
-  },
-  Remarks: { type: String, default: "Not Available" }, // default value set
+  // Mobile: {
+  //   type: String,
+  //   validate: {
+  //     validator: function(value) {
+  //      if (value.length === 11) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //     }
+  //   },
+  //   message: "Mobile number must be exactly 11 digits"
+  // }
+  // },
+  // Remarks: { type: String, default: "Not Available" }, // default value set
 },
  {versionKey:false });
 
